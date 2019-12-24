17173182d409c9c859edc91cab1d47ba53edc25e
David Holmes <phoenix1988@gmail.com>
2019-12-24 14:20:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	monopoly = {
+	monopoly_with_fourth_tier = {
 		railroads: {pieces: 4,
 			rent_in_dollars: {
 				one_piece_owned: 25,
